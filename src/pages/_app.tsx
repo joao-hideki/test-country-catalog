657e38154ea3539948014e012e98c34f0f96f8fc
joao-hideki <joaoohideki12345@gmail.com>
2023-03-30 03:54:02 -0300

    layout da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { GlobalStyle } from '@/styles/global';
+import { Header } from '@/styles/pages/app';
 import { defaultTheme } from '@/styles/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
@@ -7,6 +8,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
       <ThemeProvider theme={defaultTheme}>
+        <Header>
+          <h1>Países</h1>
+        </Header>
+
         <Component {...pageProps} />
         <GlobalStyle />
       </ThemeProvider>
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    font-size: 36px;
+    line-height: 49px;
+    margin-left: 75px;
+  }
+`;
